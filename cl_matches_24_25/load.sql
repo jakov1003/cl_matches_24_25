7bdc6382b88dd74cda784fa5a8f360d8a0b1e10c
Jakov Arambašić <jakov100113@gmail.com>
2025-05-20 10:51:51 +0200

    Update load.sql

--- cl_matches_24_25/load.sql
+++ cl_matches_24_25/load.sql
@@ -2,15 +2,15 @@
 I used the same data loading concept from my previous project and, unsurprisingly, encountered the same error.
 
 This is how I resolved it (valid for PostgreSQL and pgAdmin):
-1.Opened pgAdmin
-2.In Object Explorer (left-hand pane), navigated to the database
-3.Right-clicked its name and selected `PSQL Tool which opened a terminal window
-4. Got the absolute file path of my csv file
+1. Opened pgAdmin
+2 .In Object Explorer (left-hand pane), navigated to the database
+3 .Right-clicked its name and selected `PSQL Tool which opened a terminal window
+4. Got the absolute file path of my CSV file
 5. Pasted the following into PSQL Tool:
 \copy [file name] FROM '[file path]' WITH (FORMAT csv, HEADER true, DELIMITER ',', ENCODING 'UTF8');
 */
 
--- The final loading step was running this line of code in VS code:
+-- The final loading step was running this line of code in VS Code:
 \copy cl_match_stats FROM 'C:\Users\Jakov\Documents\cl_match_stats.csv' WITH (FORMAT csv, HEADER true,DELIMITER ',', ENCODING 'UTF8');
 
 -- If you are going to use the CSV data, you do not have to use the same loading concept as I did
